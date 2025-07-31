5abdd6af07e7b93645b12ab460e420a364bda4f1
skychx <skychx@hotmail.com>
2025-07-31 17:02:27 +0800

    feat(media-utils): init media-utils package and add Base64ImageTool (#10)
    * feat(puppeteer-enhance): init media-utils package
    * feat(meida-utils): add Base64ImageTool class
    * feat(meida-utils): add base64 test cases: browser environment simulation
    * feat(meida-utils): optimize performance for getting width/height from base64 images
    * feat(meida-utils): add webp test cases
    * feat(meida-utils): optimize performance for getting width/height from jpeg
    * feat(meida-utils): add Base64ImageTool browser test case
    * feat(meida-utils): update readme.md
    
    ---------
    
    Co-authored-by: skychx <skychx.hotmail.com>

---
+++ packages/media-utils/src/type.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2025 Bytedance, Inc. and its affiliates.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+export type ImageType = 'jpeg' | 'png' | 'webp' | 'gif' | 'bmp';
+
+export interface ImageDimensions {
+  width: number;
+  height: number;
+}
